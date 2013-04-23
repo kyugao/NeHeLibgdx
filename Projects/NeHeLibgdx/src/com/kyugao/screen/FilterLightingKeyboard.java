2cb338b8d007d8e939a50bbb21404b0467c83f04
Adam Gao <solonot@gmail.com>
2013-04-23 20:21:07 +1200

    remove unused commend, and code

--- Projects/NeHeLibgdx/src/com/kyugao/screen/FilterLightingKeyboard.java
+++ Projects/NeHeLibgdx/src/com/kyugao/screen/FilterLightingKeyboard.java
@@ -150,7 +150,6 @@ public class FilterLightingKeyboard implements Screen, InputProcessor {
 	@Override
 	public boolean keyDown(int keycode) {
 		if (keycode == Input.Keys.L && !lp) {
-			System.out.println("l key downing");
 			lp = true;
 			light = !light;
 			if (light) {
@@ -168,7 +167,6 @@ public class FilterLightingKeyboard implements Screen, InputProcessor {
 	@Override
 	public boolean keyUp(int keycode) {
 		if (keycode == Input.Keys.L && lp) {
-			System.out.println("l key up");
 			lp = false;
 		} else if (keycode == Input.Keys.F && fp) {
 			fp = false;
@@ -178,37 +176,31 @@ public class FilterLightingKeyboard implements Screen, InputProcessor {
 
 	@Override
 	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(int amount) {
-		System.out.println(amount);
 		z += amount;
 		return false;
 	}
