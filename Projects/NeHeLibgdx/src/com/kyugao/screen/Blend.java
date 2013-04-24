76404464c8046ba633d717b5679ce9d54100b8e7
Adam Gao <solonot@gmail.com>
2013-04-24 20:47:41 +1200

    use mesh instead of object file

--- Projects/NeHeLibgdx/src/com/kyugao/screen/Blend.java
+++ Projects/NeHeLibgdx/src/com/kyugao/screen/Blend.java
@@ -6,21 +6,35 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g3d.loaders.ModelLoaderRegistry;
-import com.badlogic.gdx.graphics.g3d.model.still.StillModel;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 
 public class Blend implements Screen, InputProcessor {
 
+	private Mesh squareMesh;
 	private PerspectiveCamera camera;
-	private Texture texture;
-	private float rquad = 0;
+	private Texture[] textureArray;
+	private int filter = 0;
+	private float xrot = 0, yrot = 0;
+	private float xspeed = 20, yspeed = 20;
 
-	// indicator of light and blend
+	// indicator for controlling the light
+	private boolean light;
+	private float z = -5.0f;
+
+	// Ambient Light
+	float[] ambientLight = { 0.5f, 0.5f, 0.5f, 1.0f };
+	// Diffuse Light
+	float[] diffuseLight = { 1.0f, 1.0f, 1.0f, 1.0f };
+	// Light Position
+	float[] position = { 0.0f, 0.0f, 2.0f, 1.0f };
+
+	// indicator for controlling the blending
 	private boolean blend;
-
-	private StillModel model;
 
 	@Override
 	public void render(float delta) {
@@ -30,29 +44,88 @@ public class Blend implements Screen, InputProcessor {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		Gdx.gl.glEnable(GL10.GL_DEPTH_TEST);
 		Gdx.gl.glEnable(GL10.GL_TEXTURE_2D);
-
 		Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE);
-
 		drawGLScene();
-		rquad += 20f * delta; // 每秒钟转20度，计算在每一次render的时间差里，要转的角度。比如0.5秒，那就应该转10度。
-		rquad %= 360;
+		xrot += xspeed * delta; // 每秒钟转20度，计算在每一次render的时间差里，要转的角度。比如0.5秒，那就应该转10度。
+		xrot %= 360;
+		yrot += yspeed * delta;
+		yrot %= 360;
 	}
 
 	private boolean drawGLScene() {
-
+		Gdx.gl10.glPushMatrix();
 		Gdx.gl10.glLoadIdentity();
-		Gdx.gl10.glTranslatef(0.0f, 0.0f, -7.0f);
-		Gdx.gl10.glRotatef(rquad, 1.0f, 1.0f, 1.0f);
-		texture.bind();
-		model.render();
+		Gdx.gl10.glTranslatef(0.0f, 0.0f, z);
+		Gdx.gl10.glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+		Gdx.gl10.glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+		textureArray[filter].bind();
+		Gdx.gl10.glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+		for (int i = 0; i < squareMesh.getMaxVertices() / 4; i++) {
+			squareMesh.render(GL10.GL_TRIANGLE_FAN, i * 4, 4); // 正方形的6个面分别画出.
+		}
+		Gdx.gl10.glPopMatrix();
 		return true;
 	}
 
 	@Override
 	public void show() {
-		FileHandle modelFile = Gdx.files.internal("g3d/cube.obj");
-		model = ModelLoaderRegistry.loadStillModel(modelFile);
-		texture = new Texture(Gdx.files.internal("data/textureglass.png"));
+		// init mesh
+		squareMesh = new Mesh(true, 24, 24, new VertexAttribute(Usage.Position,
+				3, "b_position"), new VertexAttribute(Usage.TextureCoordinates,
+				2, "b_texture"));
+
+		squareMesh.setVertices(new float[] {
+				// Front face:
+				-1f, 1.0f, 1.0f, 0f, 0f, // TL
+				-1f, -1.0f, 1.0f, 0f, 1f, // BL
+				1f, -1.0f, 1.0f, 1f, 1f, // BR
+				1f, 1.0f, 1.0f, 1f, 0f, // TR
+				// Top face:
+				-1f, 1.0f, -1.0f, 0f, 0f, // LR
+				-1f, 1.0f, 1.0f, 0f, 1f, // LN
+				1f, 1.0f, 1.0f, 1f, 1f, // RN
+				1f, 1.0f, -1.0f, 1f, 0f, // RR
+				// Rear face
+				1f, 1.0f, -1f, 0f, 0f, // TR
+				1f, -1.0f, -1f, 0f, 1f, // BR
+				-1f, -1.0f, -1f, 1f, 1f, // BL
+				-1f, 1.0f, -1f, 1f, 0f, // TL
+				// Bottom face
+				-1f, -1.0f, 1f, 0f, 0f, // LN
+				1f, -1.0f, 1f, 0f, 1f,// RN
+				1f, -1.0f, -1f, 1f, 1f, // RR
+				-1f, -1.0f, -1f, 1f, 0f, // LR
+				// Left face
+				-1f, 1.0f, -1f, 0f, 0f, // TR
+				-1f, 1.0f, 1f, 0f, 1f, // TN
+				-1f, -1.0f, 1f, 1f, 1f, // BN
+				-1f, -1.0f, -1f, 1f, 0f, // BR
+				// Right face
+				1f, 1.0f, 1f, 0f, 0f, // TN
+				1f, 1.0f, -1f, 1f, 0f, // TR
+				1f, -1.0f, -1f, 1f, 1f, // BR
+				1f, -1.0f, 1f, 0f, 1f // BN
+				});
+		squareMesh.setIndices(new short[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+				11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 });
+
+		// init texture
+		FileHandle imageFileHandle = Gdx.files.internal("data/Glass.bmp");
+		textureArray = new Texture[3];
+		textureArray[0] = new Texture(imageFileHandle);
+		textureArray[0].setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
+		textureArray[1] = new Texture(imageFileHandle);
+		textureArray[1].setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		textureArray[2] = new Texture(imageFileHandle, true);
+		textureArray[2].setFilter(TextureFilter.MipMap, TextureFilter.Linear);
+
+		// init light
+		Gdx.gl.glEnable(GL10.GL_LIGHT1);
+		Gdx.gl10.glLightfv(GL10.GL_LIGHT1, GL10.GL_AMBIENT, ambientLight, 0);
+		Gdx.gl10.glLightfv(GL10.GL_LIGHT1, GL10.GL_DIFFUSE, diffuseLight, 0);
+		Gdx.gl10.glLightfv(GL10.GL_LIGHT1, GL10.GL_POSITION, position, 0);
+
+		// Set input processor
 		Gdx.input.setInputProcessor(this);
 	}
 
@@ -80,7 +153,16 @@ public class Blend implements Screen, InputProcessor {
 
 	@Override
 	public boolean keyDown(int keycode) {
-		if (keycode == Input.Keys.B) {
+		if (keycode == Input.Keys.L) {
+			light = !light;
+			if (light) {
+				Gdx.gl.glEnable(GL10.GL_LIGHTING);
+			} else {
+				Gdx.gl.glDisable(GL10.GL_LIGHTING);
+			}
+		} else if (keycode == Input.Keys.F) {
+			filter = (filter + 1) % 3;
+		} else if (keycode == Input.Keys.B) {
 			System.out.println("B down");
 			blend = !blend;
 			if (blend) {
@@ -90,6 +172,15 @@ public class Blend implements Screen, InputProcessor {
 				Gdx.gl.glEnable(GL10.GL_DEPTH_TEST);
 				Gdx.gl.glDisable(GL10.GL_BLEND);
 			}
+		} else if (keycode == Input.Keys.DOWN) {
+
+			xspeed += 1f;
+		} else if (keycode == Input.Keys.UP) {
+			xspeed -= 1f;
+		} else if (keycode == Input.Keys.LEFT) {
+			yspeed -= 1f;
+		} else if (keycode == Input.Keys.RIGHT) {
+			yspeed += 1f;
 		}
 		return false;
 	}
@@ -126,6 +217,7 @@ public class Blend implements Screen, InputProcessor {
 
 	@Override
 	public boolean scrolled(int amount) {
+		z += amount;
 		return false;
 	}
 
